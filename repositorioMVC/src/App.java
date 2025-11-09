f50a17bde54c3228276a8aa850f99bcbf7f01624
jorgeuao <jorgeuao.a@gmail.com>
2025-11-09 18:53:52 -0500

    He empezado 4

--- repositorioMVC/src/App.java
+++ repositorioMVC/src/App.java
@@ -2,17 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controlador;
 
 /**
  *
  * @author jorgeuao
  */
-public class controladorInventario {
+
+import controlador.controladorInventario; 
+
+public class App {
     
-    
-    public void prueba(){
-        System.out.println("hola");
+    public static void main(String[] args) {
+        
+        controladorInventario control = new controladorInventario(); 
+        
+        control.prueba();
     }
     
 }
